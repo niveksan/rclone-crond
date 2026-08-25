fb1f255a5c29db660a836675dfbe54e2bec41a53
niveksan <werbekev@hotmail.com>
2026-08-25 19:31:38 +0200

    Chunked Upload

--- backup.sh
+++ backup.sh
@@ -10,5 +10,5 @@ rclone sync -v --stats-one-line $THROTTLE Local:/data/homeassistant/backups Next
 
 #Nextcloud
 #rclone move -v --stats-one-line --bwlimit=800k:off Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
-#ohne Limit
-rclone move -v --stats-one-line $THROTTLE Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
+
+rclone move -v --stats-one-line --webdav-nextcloud-chunk-size 100M $THROTTLE Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
