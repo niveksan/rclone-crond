2f6b38eb351bbd1333f6167727763c4906166c30
niveksan <werbekev@hotmail.com>
2026-08-26 09:06:59 +0200

    deaktivere Backup nach Container Start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
-# run backup once on container start to ensure it works
-/backup.sh
+# uncomment to run backup once on container start to ensure it works
+#/backup.sh
+
 # start crond in foreground
 exec crond -f
